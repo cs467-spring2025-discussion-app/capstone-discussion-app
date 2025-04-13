f206ef52beec6981921636f5602805e1f702842c
al-ce <aceserani@gmail.com>
2025-04-13 16:46:24 -0500

    test(auth/models/session): deleting a user should delete associated sessions

--- src/auth/internal/models/sessionModel_test.go
+++ src/auth/internal/models/sessionModel_test.go
@@ -8,9 +8,12 @@ import (
 	"github.com/matryer/is"
 
 	"godiscauth/internal/models"
+	"godiscauth/internal/testutils"
 	"godiscauth/pkg/apperrors"
 )
 
+// TestSessionModel_NewSession tests new Session creation in the `models`
+// package
 func TestSessionModel_NewSession(t *testing.T) {
 	is := is.New(t)
 
@@ -34,5 +37,50 @@ func TestSessionModel_NewSession(t *testing.T) {
 	t.Run("fails when expiration time is empty", func(t *testing.T) {
 		_, err := models.NewSession(uuid.New(), "test-token", time.Time{})
 		is.Equal(err, apperrors.ErrExpiresAtIsEmpty)
+	})
+}
+
+// TestSessionModel_CascadeToSessions tests that deleting a user in the
+// database scrubs any associated sessions by OnDelete-Cascade
+func TestSessionModel_CascadeToSessions(t *testing.T) {
+	testDB := testutils.TestDBSetup()
+	is := is.New(t)
+
+	t.Run("user sessions are deleted when user is deleted", func(t *testing.T) {
+		tx := testDB.Begin()
+		defer tx.Rollback()
+
+		// Create test user for sessions (required foreign key reference)
+		userName := "testCascadeDeleteSessions@test.com"
+		testUser, err := models.NewUser(userName, testutils.TestingPassword)
+		is.NoErr(err)
+
+		err = tx.Create(testUser).Error
+		is.NoErr(err)
+
+		// Create test sessions referencing the test user
+		for range 3 {
+			session, err := models.NewSession(
+				testUser.ID,
+				uuid.New().String(),
+				time.Now().Add(1*time.Hour),
+			)
+			is.NoErr(err)
+			err = tx.Create(session).Error
+			is.NoErr(err)
+		}
+
+		// Check sessions are created
+		var count int64
+		tx.Model(&models.Session{}).Where("user_id = ?", testUser.ID).Count(&count)
+		is.Equal(count, int64(3))
+
+		// Delete the user
+		result := tx.Unscoped().Where("id = ?", testUser.ID).Delete(&models.User{})
+		is.Equal(result.RowsAffected, int64(1))
+
+		// Expect all sessions deleted
+		tx.Model(&models.Session{}).Where("user_id = ?", testUser.ID).Count(&count)
+		is.Equal(count, int64(0))
 	})
 }
