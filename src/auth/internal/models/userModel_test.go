b3cdb2f297f9eb2fc272b411d2bf7d0199ae007d
al-ce <aceserani@gmail.com>
2025-04-13 14:37:48 -0500

    test(auth/models/user): username cannot exceeds 254 characters
    If using email as username, then username should not exceed 254
    characters
    
    https://www.rfc-editor.org/errata/eid1690

--- src/auth/internal/models/userModel_test.go
+++ src/auth/internal/models/userModel_test.go
@@ -19,4 +19,11 @@ func TestNewUser(t *testing.T) {
 		_, err := models.NewUser(validEmail, validPassword)
 		is.NoErr(err)
 	})
+
+	t.Run("email exceeds 254 chars", func(t *testing.T) {
+		exceeds254Chars := "a" + string(make([]byte, 253)) + "@example.com"
+		validPassword := testutils.TestingPassword
+		_, err := models.NewUser(exceeds254Chars, validPassword)
+		is.True(err != nil)
+	})
 }
