326d8909bb82622d91076f6d1da9822f53fb1edf
al-ce <aceserani@gmail.com>
2025-04-13 19:20:36 -0500

    feat(auth/apperrors): organize errors; add session errors

--- src/auth/pkg/apperrors/apperrors.go
+++ src/auth/pkg/apperrors/apperrors.go
@@ -7,18 +7,26 @@ import (
 var New = errors.New
 
 var (
+	// User registration errors
 	ErrDuplicateEmail = New("Email already exists in database")
+	ErrEmailIsEmpty   = New("Email is empty")
 	ErrEmailMaxLength = New("Email exceeds max length of 254 characters")
 
+	// User registration errors
+	ErrSessionAlreadyExists = New("Session already exists")
+
+	// Nil reference argument errors
 	ErrDatabaseIsNil = New("Database is nil")
+	ErrSessionIsNil  = New("Session is nil")
 	ErrUserIsNil     = New("User is nil")
 
-	ErrEmailIsEmpty     = New("Email is empty")
+	// Empty string argument errors
 	ErrExpiresAtIsEmpty = New("Expiration time is empty")
 	ErrPasswordIsEmpty  = New("Password is empty")
 	ErrTokenIsEmpty     = New("Token is empty")
 	ErrUserIdEmpty      = New("User ID is empty")
 
+	// Database errors
 	ErrUserNotFound = New("User not found")
 
 	ErrCouldNotIncrementFailedLogins = New("Could not increment users.failed_login_attempts")
