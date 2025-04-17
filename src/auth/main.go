e191f027fdcadf98649ee071a85f548a9a09572e
al-ce <aceserani@gmail.com>
2025-04-16 20:34:02 -0500

    feat(auth): enforce SESSION_SECRET complexity

--- src/auth/main.go
+++ src/auth/main.go
@@ -5,14 +5,21 @@ import (
 	"os"
 
 	"github.com/rs/zerolog/log"
+	passwordvalidator "github.com/wagslane/go-password-validator"
 
 	"godiscauth/internal/database"
 	"godiscauth/internal/server"
+	"godiscauth/pkg/config"
 	"godiscauth/pkg/logger"
 )
 
 // main is the entry point for the auth service. It sets up the logger, connects to the database, and starts the API server.
 func main() {
+	// SESSION_SECRET must be complex for encryption
+	if err := passwordvalidator.Validate(os.Getenv(config.SessionCookieName), config.MinEntropyBits); err != nil {
+		log.Fatal().Err(err).Msg("Session secret is not complex enough")
+	}
+
 	logger.SetupLogger()
 	db, err := database.NewDB()
 	if err != nil {
